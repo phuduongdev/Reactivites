c1b6d72e1e4410296f8df496dde33281076d476a
phuduongdev <phu.duongtan@gmail.com>
2020-07-07 14:11:03 +0700

    Add project files.

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase {
+        private readonly DataContext context;
+
+        public ValuesController(DataContext context) {
+            this.context = context;
+        }
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get() {
+            var values = await context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Value>> Get(int id) {
+            var value = await context.Values.FindAsync(id);
+            return Ok(value);
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value) {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value) {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id) {
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,45 @@
+# Building an App with Dotnet Core and React
+## Technologies
+- ASP.NET Core 3.1.x
+- Entity Framework Core 3.1.x
+
+## Clean Architecture
+
+                 API
+                  |
+                  v
+              Application
+             /    |
+            /     v
+           /    Domain
+          /     ^
+         v     /
+    Persistence
+
+## Project Structure
+
+Project
+|**Domain : library, contains the bussiness entities
+|**Application : library, process the bussiness logics
+|**Persistence : library, access to the database
+|**API : Web Api App, recive HTTP requests and respond to them
+|\_\_Project.sln
+
+## NuGet packages
+
+- Microsoft.EntityFrameworkCore
+- Microsoft.EntityFrameworkCore.Sqlite
+- third
+
+## Dotnet CLI (Window command):
+
+- dotnet new sln
+- dotnet new classlib -n project_name
+- dotnet new webapi -n project_name
+- dotnet sln add project_name
+- dotnet add reference ...
+  - Domain: none
+  - Application: reference to Domain, Persistence
+  - Persistence: reference to Domain
+  - API: reference to Application
+- dotnet ef migrations add Initial -p .\Persistence\ -s .\API\
